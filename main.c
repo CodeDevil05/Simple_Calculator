42cfb482d39a6b676d807b57c6c7b50ffc39c958
lochan mahesh <130279114+CodeDevli@users.noreply.github.com>
2023-09-03 23:40:07 +0530

    Update main.c

--- main.c
+++ main.c
@@ -3,18 +3,15 @@
 int main() {
     char operator;
     double num1, num2, result;
-
-    // Input from the user
     printf("Enter the first number: ");
     scanf("%lf", &num1);
 
     printf("Enter an operator (+, -, *, /): ");
-    scanf(" %c", &operator); // Note the space before %c to consume the newline character.
+    scanf(" %c", &operator); 
 
     printf("Enter the second number: ");
     scanf("%lf", &num2);
 
-    // Perform the calculation based on the operator
     switch (operator) {
         case '+':
             result = num1 + num2;
@@ -30,16 +27,13 @@ int main() {
                 result = num1 / num2;
             } else {
                 printf("Error: Division by zero is not allowed.\n");
-                return 1; // Return non-zero to indicate an error.
+                return 1; 
             }
             break;
         default:
             printf("Error: Invalid operator entered.\n");
-            return 1; // Return non-zero to indicate an error.
+            return 1; 
     }
-
-    // Output the result
     printf("Result: %.2f\n", result);
-
-    return 0; // Return zero to indicate successful execution.
+     return 0; /
 }
